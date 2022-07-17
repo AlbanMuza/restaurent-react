8e51b5b822420a30e3f73a40a31c9de330d75082
Alban Muzanenhamo <38377066+AlbanMuza@users.noreply.github.com>
2022-07-17 23:36:46 +0200

    first get

---
+++ src/container/AboutUs/AboutUs.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { images } from '../../constants';
+import './AboutUs.css';
+
+const AboutUs = () => (
+  <div className="app__aboutus app__bg flex__center section__padding" id="about">
+    <div className="app__aboutus-overlay flex__center">
+      <img src={images.G} alt="G_overlay" />
+    </div>
+
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">About Us</h1>
+        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        <p className="p__opensans">Sit in the beautiful gardans at Gallagher Convention Centre, the Wine Garden is an urban gem.The tranquil setting is delight that allows guests to appreciate the birdlife surrounding them. Don't miss the chance to escape the urban jungle and enjoy the peace and tranquillity in thw Wines Garden  </p>
+        <button type="button" className="custom__button">Know More</button>
+      </div>
+
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt="about_knife" />
+      </div>
+
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">Our History</h1>
+        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        <p className="p__opensans">Gaallanger Wine Gardens is one of the best wine Gardens in Gauteng. It was built in 1998 and until this day on the people of south africe consider it one of the best wine Gardens  .</p>
+        <button type="button" className="custom__button">Know More</button>
+      </div>
+    </div>
+  </div>
+);
+
+export default AboutUs;
